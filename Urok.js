7fb349286ac081b886bf5bf9b17f14babb54f3bd
Slava <slavap333a@mail.ru>
2019-10-04 17:11:30 +0300

    first commit

---
+++ Urok.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/* let peremennaya = 10; // Присваивание значения
+
+console.log("2" == 2);  // Сравнение зеачений: Первый тип данных, это строка, второй число, ответ true.
+console.log("2" === 2); // Сравнение типов данных, ответ false
+console.log(peremennaya);
+
+*/
+
+let isChecked = true,
+    isClose = false;
+
+    console.log(isChecked && isClose);
+    console.log(isClose || isChecked);
+    console.log(isChecked && !isClose);
